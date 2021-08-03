a31e52e7b4f81adfbace02fbb89b5656b9e7c177
trungtdd <duytrungdng@gmail.com>
2021-08-03 13:29:40 +0700

    trigger unittest failed for CICD learning

--- django_authen_api/todo_list/tests/test_account.py
+++ django_authen_api/todo_list/tests/test_account.py
@@ -48,7 +48,7 @@ class AccountTests(APITestCase):
             "password" : "12345678"
         }
         response = self.client.post(url, data=body, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_101_SWITCHING_PROTOCOLS)
 
 
 
